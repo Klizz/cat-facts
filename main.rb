ccfdede79b2601357f7cd69ac8b7a14391e6eeb4
Karen <klizzy@hotmail.es>
2020-10-09 17:58:59 -0500

    Added main.rb

---
+++ main.rb
@@ -0,0 +1,26 @@
+# http_server.rb
+require 'socket'
+server = TCPServer.new 5678
+
+require 'net/http'
+require 'json'
+
+key = "68db822c-1fce-4432-ab1b-fb7457d37562"
+
+url = 'https://api.thecatapi.com/v1/images/search'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+body = JSON.parse(response)
+puts body
+
+while session = server.accept
+  request = session.gets
+  puts request
+
+  session.print "HTTP/1.1 200\r\n" # 1
+  session.print "Content-Type: json\r\n" # 2
+  session.print "\r\n" # 3
+  session.print body["text"] #4
+
+  session.close
+end
